1c2fe29d06d6d1d6f0903c8a99d4786a8b8e7d18
Azat Khuzhin <a3at.mail@gmail.com>
2013-07-24 01:35:39 +0400

    [bash] add git_ahead_behind() helper

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -1,4 +1,12 @@
 alias git-changelog='git --no-pager log --format="%ai %aN %n%n%x09* %s%d%n"'
 
+function git_ahead_behind
+{
+    curr_branch=$(git rev-parse --abbrev-ref HEAD);
+    curr_remote=$(git config branch.$curr_branch.remote);
+    curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
+    git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch | tr -s '\t' '|';
+}
+
 GIT_REVIEW_DIFF=$(git config diff.tool) : ${GIT_REVIEW_DIFF:="git diff"}
 export GIT_REVIEW_DIFF
