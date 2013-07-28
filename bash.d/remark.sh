376aba0172b581f189a65bbadbf06f25811391e3
Azat Khuzhin <a3at.mail@gmail.com>
2013-07-29 00:55:26 +0400

    [bash] remark: add "c" prefix for command aliases

--- bash.d/remark.sh
+++ bash.d/remark.sh
@@ -2,6 +2,6 @@
 
 BASE_CONFIG_PATH=/usr/share/regex-markup
 
-ping() { `which ping` $@ | remark $BASE_CONFIG_PATH/ping; }
-traceroute() { `which traceroute` $@ | remark $BASE_CONFIG_PATH/traceroute; }
+cping() { `which ping` $@ | remark $BASE_CONFIG_PATH/ping; }
+ctraceroute() { `which traceroute` $@ | remark $BASE_CONFIG_PATH/traceroute; }
 
