5f29962c4c5603195f9fa58aaed1a709e3f5bfce
Azat Khuzhin <a3at.mail@gmail.com>
2013-08-08 15:06:01 +0400

    [bash] git_ahead_behind: drop extra semicolon at the end of line

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -1,18 +1,18 @@
 function git_ahead_behind
 {
-    curr_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null);
-    curr_remote=$(git config branch.$curr_branch.remote);
+    curr_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    curr_remote=$(git config branch.$curr_branch.remote)
     if [ -z $curr_remote ]; then
         echo "0|0"
         return
     fi
-    curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
+    curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3)
     diff=$(git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch 2>/dev/null)
     if [ -z "$diff" ]; then
         echo "N"
         return
     fi
-    echo "$diff" | tr -s '\t' '|';
+    echo "$diff" | tr -s '\t' '|'
 }
 
 GIT_REVIEW_DIFF=$(git config diff.tool) : ${GIT_REVIEW_DIFF:="git diff"}
