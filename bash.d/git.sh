0e705ce94a3102fbec26c96d8100881aac831f1e
Azat Khuzhin <a3at.mail@gmail.com>
2013-08-08 15:05:24 +0400

    [bash] git_ahead_behind: avoid printing errors if there only one commit

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -7,7 +7,12 @@ function git_ahead_behind
         return
     fi
     curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
-    git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch | tr -s '\t' '|';
+    diff=$(git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch 2>/dev/null)
+    if [ -z "$diff" ]; then
+        echo "N"
+        return
+    fi
+    echo "$diff" | tr -s '\t' '|';
 }
 
 GIT_REVIEW_DIFF=$(git config diff.tool) : ${GIT_REVIEW_DIFF:="git diff"}
