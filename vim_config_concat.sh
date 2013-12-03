1464b513846ab8f1f4309b34215f92e07b043017
Azat Khuzhin <a3at.mail@gmail.com>
2013-12-03 14:40:33 +0400

    vim: config concat: add date/version into it

--- vim_config_concat.sh
+++ vim_config_concat.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+echo "#"
+echo "# Generated at $(date)"
+echo "# Version: $(git describe 2>/dev/null || git sha1)"
+echo "# From https://github.com/azat/dot_files"
+echo "#"
+
 sed 's/\/etc\/vim/~\/.vim/g' ./vim/vimrc
+
