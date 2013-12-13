152cdeba0e035db40add6a7d1ce5dd23706617d5
Azat Khuzhin <a3at.mail@gmail.com>
2013-12-13 16:30:54 +0400

    bash: add tmuxcatwindow function

--- bash.d/tmux.sh
+++ bash.d/tmux.sh
@@ -19,3 +19,12 @@ ssh() {
 	settitle "bash"
 }
 
+# $1 - session:windowNum
+tmuxcatwindow()
+{
+	session_window="$1"
+	start=${2:-"-1000000000"}
+	[ -n "$session_window" ] || return
+	(for i in $(tmux list-pane -t "$session_window" | awk -F: '{print $1}'); do tmux -q capture-pane -t "$session_window".$i -p -S "$start"; done)
+}
+
