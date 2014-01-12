afcea2239ceaaafc801ade18ba90eb8d1dd10fd6
Azat Khuzhin <a3at.mail@gmail.com>
2014-01-12 20:48:31 +0400

    gdb: add gdb_leaking helper

---
+++ gdb/gdb_leaking.py
@@ -0,0 +1,38 @@
+#
+# TODO: use class instead of command
+# requires ./gdb_memory.py
+#
+# Try to investigate memory leaks,
+# run leaking when youu program must _free_ some mem.
+# (was written to investigate leaking in tmux)
+#
+
+import gdb
+import re
+
+class leaking(gdb.Command):
+    def __init__(self):
+        gdb.Command.__init__ (self, "leaking", gdb.COMMAND_OBSCURE)
+
+    def invoke(self, arg, from_tty):
+        gdb.execute("break free")
+
+        max = 1 << 24
+        i = 0
+        prev = self.memory()
+        print "Start: %lu bytes" % (prev)
+
+        while ++i < max:
+            current = self.memory()
+            if ((prev - current) > prev * 0.1):
+                print "Current: %lu bytes, was %lu bytes (%i iteration)" % (current, prev, i)
+                break
+
+            gdb.execute("continue")
+
+    def memory(self):
+        mem = gdb.execute("info memory", False, True)
+        return int(re.search("\(([0-9]*) bytes\)", mem).group(1))
+
+leaking()
+
