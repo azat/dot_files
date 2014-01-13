1c71df01d291bf7aaf66b6fc15d2b234b5b1cbe5
Azat Khuzhin <a3at.mail@gmail.com>
2014-01-13 16:37:21 +0400

    gdb/leaking: use malloc_usable_size() instead

--- gdb/gdb_leaking.py
+++ gdb/gdb_leaking.py
@@ -15,37 +15,47 @@ class leaking(gdb.Command):
         gdb.Command.__init__ (self, "leaking", gdb.COMMAND_OBSCURE)
 
     def invoke(self, arg, from_tty):
+        # delete all breakpoints
+        gdb.execute("d")
         gdb.execute("break free")
+        # loop iterations only in free()
+        gdb.execute("continue")
 
         max = 1 << 24
         i = 0
-        prev = self.memory()
-        print "Start: %lu bytes (max iterations: %lu)" % (prev, max)
+
+        initial = self.memory()
 
         for i in range(max):
             try:
-                current = self.memory()
+                mem = gdb.selected_frame().read_var("mem")
+                current = self.mallocUsableSize(mem)
             except gdb.error:
                 break
 
-            if ((prev - current) > prev * 0.1):
-                print "Current: %lu bytes, was %lu bytes (%i iteration)" % (current, prev, i)
-                print "Backtrace before current free():\n%s" % (prevBt)
-                break
+            bt = gdb.execute("bt", False, True)
 
-            prevBt = gdb.execute("bt", False, True)
+            # 5 %
+            if (current > initial*0.05):
+                print "Current: %lu bytes, initial %lu bytes (%i iteration)" % (current, initial, i)
+                print "Backtrace before current free():\n%s" % (bt)
+                return
+
             if ((i % 100000) == 0):
-                print "Iteration %i backtrace:\n%s" % (i, prevBt)
+                print "Iteration %i backtrace:\n%s" % (i, bt)
 
             gdb.execute("continue", False, True)
 
-        print "Finished with: %lu bytes, was %lu bytes (%i iteration)" % (current, prev, i)
-        print "Backtrace before current free():\n%s" % (prevBt)
+        print "Finished with %i iterations" % (i)
+        print "Last backtrace:\n%s" % (bt)
 
     def memory(self):
         # VmSize:   111784 kB
         mem = gdb.execute("info proc status", False, True)
         return int(re.search("VmSize:\s*([0-9]*) kB", mem).group(1))*1024
+    def mallocUsableSize(self, ptr):
+        mem = gdb.execute("p malloc_usable_size(%lu)" % ptr, False, True)
+        return int(re.search("= ([0-9]*)", mem).group(1))
 
 leaking()
 
