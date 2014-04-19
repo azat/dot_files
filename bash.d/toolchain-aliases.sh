03292129324816ee255ae8024b52ae6db02f2fca
Azat Khuzhin <a3at.mail@gmail.com>
2014-04-19 20:39:39 +0400

    bash: add c_const() functin, to search c/cxx consts in headers

--- bash.d/toolchain-aliases.sh
+++ bash.d/toolchain-aliases.sh
@@ -5,3 +5,18 @@ function cm()
     make $* > /dev/null
 }
 
+function c_const()
+{
+    const="$1"
+    additional_headers="$2"
+    headers=$(cat <<-EOL
+        stdint.h stdio.h stdlib.h unistd.h fcntl.h \
+        sys/types.h sys/stat.h \
+        sys/socket.h netinet/in.h arpa/inet.h
+EOL)
+    (
+        for header in $additional_headers $headers; do
+            printf "#include <%s>\n" "$header"
+        done
+    ) | g++ -E -dM -xc++ - | fgrep $1 | fgrep define
+}
