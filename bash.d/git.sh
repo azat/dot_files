65be77a84fa5407dd1fda14505aa34bcbf9f6c40
Azat Khuzhin <a3at.mail@gmail.com>
2014-08-06 19:16:44 +0400

    bash: git_avg: we must drop first two lines for commit description

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -71,7 +71,7 @@ git_avg()
 {
     for i in $(git log --format=%h "$@"); do
         echo -n "$i " >& 2
-        git log --format=%B $i -1 | tee -a >(head -1) | tail -n+2 | wc
+        git log --format=%B $i -1 | tee -a >(head -1) >(tail -n+3) >/dev/null | wc
     done |& awk '\
 {
     if (!$2) {
