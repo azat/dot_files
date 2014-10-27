3353330038e3c9e39996ac23e1cc29c9fa7f60c8
Azat Khuzhin <a3at.mail@gmail.com>
2014-10-28 00:27:00 +0300

    bash.d/aliases: map 'strings' to 'strings -a' to avoid use of buggy libbfd
    For more info read this:
    http://lcamtuf.blogspot.ca/2014/10/psa-dont-run-strings-on-untrusted-files.html

--- bash.d/aliases.sh
+++ bash.d/aliases.sh
@@ -14,3 +14,4 @@ alias tm='tmux attach || tmux new'
 alias sortfast="sort -S$(($(sed '/MemT/!d;s/[^0-9]*//g' /proc/meminfo)/1024-200)) --parallel=$(($(grep -c ^proc /proc/cpuinfo)*2))"
 alias save_kde_session='qdbus org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.saveCurrentSession'
 alias f='find ./ -name'
+alias strings='strings -a'
