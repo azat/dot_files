9799408635ba5328e65de7f4a319721767b0ca9d
Azat Khuzhin <a3at.mail@gmail.com>
2015-04-05 19:20:20 +0300

    gdb: add ninja command (like make builtin, and reload binary on it)

---
+++ gdb/gdb_ninja.py
@@ -0,0 +1,24 @@
+#
+# Why only "make" works inside gdb?
+#
+# Command:
+# ninja ...
+#
+
+import gdb
+from subprocess import call
+
+class ninja(gdb.Command):
+    def __init__(self):
+        gdb.Command.__init__(self, "ninja", gdb.COMMAND_OBSCURE)
+
+    def invoke(self, arg, from_tty):
+        call("ninja " + arg, shell=True)
+        for o in gdb.objfiles():
+            print "Reload %s" % (o.filename)
+            self.reload(o.filename)
+
+    def reload(self, filename):
+        gdb.execute("symbol-file %s" % (filename))
+
+ninja()
