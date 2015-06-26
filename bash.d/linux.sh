679790012a5dc5045073a0c1fcce9b5e683d1654
Azat Khuzhin <a3at.mail@gmail.com>
2015-06-26 11:52:51 +0300

    bash: linux_ctags

---
+++ bash.d/linux.sh
@@ -0,0 +1,10 @@
+function linux_ctags()
+{
+    local arch=${1:-"x86"}
+
+    eval ctags --recurse \
+        "$(ls -1d arch/* | fgrep -v $arch | xargs printf '--exclude=%s/* ')" \
+        --exclude='drivers/*' \
+        --exclude=build*/* \
+        --exclude=sound/* .
+}
