499c2b8606f29830b0df40bf40999c8fbb9696a0
Azat Khuzhin <a3at.mail@gmail.com>
2015-09-06 23:55:59 +0300

    bash/gpp: adopt for 1.4.19 (gpg agent socket in home dir)

--- bash.d/gpg.sh
+++ bash.d/gpg.sh
@@ -2,5 +2,10 @@ export GPG_TTY=$(tty)
 
 function restore_gpg_agent()
 {
-    export GPG_AGENT_INFO=$(find /tmp/gpg-*/S.gpg-agent -user $USER):$(pgrep -u$USER gpg-agent):1
+    local home=/home/azat/.gnupg/S.gpg-agent
+    if [ -e $home ]; then
+        export GPG_AGENT_INFO=$home:0:1
+    else
+        export GPG_AGENT_INFO=$(find /tmp/gpg-*/S.gpg-agent -user $USER):$(pgrep -u$USER gpg-agent):1
+    fi
 }
