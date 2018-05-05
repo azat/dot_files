azat/dot_files

commit 3fca24fd1224b5e506f13073ce993851a3e93f1d

bash: remove hackish helpers for restoring ssh/gpg agent sock
Instead I should use init.d/systemd-service, to make undependent from X
server.

bash.d/gpg.sh
@@ -1,14 +1,0 @@
-export GPG_TTY=$(tty)
-
-function restore_gpg_agent()
-{
-    local home=/home/azat/.gnupg/S.gpg-agent
-    local run=/run/user/$UID/gnupg/S.gpg-agent
-    if [ -e $run ]; then
-        export GPG_AGENT_INFO=$run:0:1
-    elif [ -e $home ]; then
-        export GPG_AGENT_INFO=$home:0:1
-    else
-        export GPG_AGENT_INFO=$(find /tmp/gpg-*/S.gpg-agent -user $USER):$(pgrep -u$USER gpg-agent):1
-    fi
-}

bash.d/ssh.sh
@@ -1,12 +1,0 @@
-# Useful in the next cases:
-# - your X is buggy
-# - you have X, but now you are run some stuff from the remote shell
-# - you restart your agent
-function restore_ssh_agent()
-{
-    local pid=$(pgrep -u$USER ssh-agent | head -1)
-    local socket=$(fgrep -af <(sudo find -L /proc/$pid/fd -type s -exec readlink -f {} \; | awk -F'[\\[\\]:]' '{print $(NF-1)}') /proc/net/unix | awk '/agent/ {print $NF; exit(0);}')
-
-    export SSH_AGENT_PID=$pid
-    export SSH_AUTH_SOCK=$socket
-}

commit 7b83d94f7b890d8985a2dbeafad6535d1defd238

bash.d/gpg: set GPG_TTY
Fixes: 3fca24fd1224b5e506f13073ce993851a3e93f1d ("bash: remove hackish
helpers for restoring ssh/gpg agent sock")

bash.d/gpg.sh
@@ -1,0 +1,2 @@
+# https://wiki.archlinux.org/index.php/GnuPG#SSH_agent
+export GPG_TTY=$(tty)