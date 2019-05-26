73a2477403dab0443e1189280c605e1ea6a28025
Azat Khuzhin <a3at.mail@gmail.com>
2019-05-27 00:29:18 +0300

    bash/history: ignore stuff that starts with non-ascii
    This will backup from accidential pasting some cyrilics and so on.

--- bash.d/history.sh
+++ bash.d/history.sh
@@ -2,7 +2,7 @@ export HISTSIZE=100000
 export HISTFILESIZE=100000
 # don't put duplicate lines in the history. See bash(1) for more options
 export HISTCONTROL="ignoreboth:erasedups"
-export HISTIGNORE="&:ls:[bf]g:exit:history:cat:l:ll:ps:history *:pwd:free:w:jobs:* --help:* -help:* -version:* --version"
+export HISTIGNORE="&:ls:[bf]g:exit:history:cat:l:ll:ps:history *:pwd:free:w:jobs:* --help:* -help:* -version:* --version:[^0-9a-zA-Z_[(]*"
 
 # all history in 1
 shopt -s histappend
