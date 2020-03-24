0aa29a1df6e73e86fa82c50dbb69a69becad4dad
Azat Khuzhin <a3at.mail@gmail.com>
2020-03-24 22:47:33 +0300

    bash: write history only if exit status != 127 (command not found)
    This will avoid saving command with typos.
    
    Although it will have some false-positive, but they are not ignored (not
    in HISTIGNORE) so this the command line can be fixed (install missing
    package) and repeated.

--- bash.d/01-history.sh
+++ bash.d/01-history.sh
@@ -6,7 +6,11 @@ export HISTIGNORE="&:ls:[bf]g:exit:history:cat:l:ll:ps:history *:pwd:free:w:jobs
 
 # all history in 1
 shopt -s histappend
-PROMPT_COMMAND+='history -a ;'
+# CUSTOM_PROMPT_LAST_COMMAND_STATUS is the status of the last command (written
+# in zzz-custom-prompt.sh), and 127 exit code is when command not found (or
+# some problems with shard libraries, but anyway they are not ignored so the
+# problem can be fixed and the command can be repeated).
+PROMPT_COMMAND+='[ -v CUSTOM_PROMPT_LAST_COMMAND_STATUS ] && [[ $CUSTOM_PROMPT_LAST_COMMAND_STATUS -ne 127 ]] && history -a ; '
 #many strings commands
 shopt -s cmdhist
 shopt -s histverify
