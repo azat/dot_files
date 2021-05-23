386573acd515522529a5713035194d0932e19b6e
Azat Khuzhin <a3at.mail@gmail.com>
2021-05-23 13:06:10 +0300

    bash: add some comments for problems with history

--- bash.d/01-history.sh
+++ bash.d/01-history.sh
@@ -1,16 +1,26 @@
 export HISTSIZE=100000
 export HISTFILESIZE=100000
-# don't put duplicate lines in the history. See bash(1) for more options
 export HISTCONTROL="ignoreboth:erasedups"
 export HISTIGNORE="&:ls:[bf]g:exit:history:cat:l:ll:ps:history *:pwd:free:w:jobs:* --help:* -help:* -version:* --version:[^0-9a-zA-Z_[{(#./~]*"
 
-# all history in 1
 shopt -s histappend
+shopt -s cmdhist
+shopt -s histverify
+
 # CUSTOM_PROMPT_LAST_COMMAND_STATUS is the status of the last command (written
 # in zzz-custom-prompt.sh), and 127 exit code is when command not found (or
 # some problems with shard libraries, but anyway they are not ignored so the
 # problem can be fixed and the command can be repeated).
-PROMPT_COMMAND+='[ -v CUSTOM_PROMPT_LAST_COMMAND_STATUS ] && [[ $CUSTOM_PROMPT_LAST_COMMAND_STATUS -ne 127 ]] && history -a ;'
-#many strings commands
-shopt -s cmdhist
-shopt -s histverify
+PROMPT_COMMAND+='[ -v CUSTOM_PROMPT_LAST_COMMAND_STATUS ] && [[ $CUSTOM_PROMPT_LAST_COMMAND_STATUS -ne 127 ]]; '
+# "history -a" is not compatible with "erasedups",
+# i.e. eventually the file will have duplicates.
+#
+# There is a "workaround" for this problem:
+#     "history -n; history -w; history -c; history -r"
+#
+# But this has the following drawbacks:
+# - "history -c" will clear the $HISTFILE (i.e. if it will be executed from shell)
+# - it will not be enough to edit $HISTFILE and remove some entry from this
+#   (i.e. entry with the password), since now it first reads the file.
+# - this will overlap history from different shells (and this is not what I want)
+PROMPT_COMMAND+='history -a ;'
