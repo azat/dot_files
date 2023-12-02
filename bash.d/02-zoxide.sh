4bf04853a49183cdb543a1a92dde11969aa219d0
Azat Khuzhin <a3at.mail@gmail.com>
2023-12-02 08:29:53 +0100

    bash: fix zoxide excludes

--- bash.d/02-zoxide.sh
+++ bash.d/02-zoxide.sh
@@ -1,3 +1,3 @@
 eval "$(zoxide init bash --cmd j --hook prompt)"
-export _ZO_EXCLUDE_DIRS="${_ZO_EXCLUDE_DIRS:+$_ZO_EXCLUDE_DIRS:}"~azat/Downloads
+export _ZO_EXCLUDE_DIRS="${_ZO_EXCLUDE_DIRS:+$_ZO_EXCLUDE_DIRS:}"'~azat/Downloads/*'
 export _ZO_EXCLUDE_DIRS="${_ZO_EXCLUDE_DIRS:+$_ZO_EXCLUDE_DIRS:}"'/tmp/*'
