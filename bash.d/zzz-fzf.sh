16c098f2776f67d5b9ad8e8c82d425ac2b6d3437
Azat Khuzhin <a3at.mail@gmail.com>
2024-05-21 10:57:17 +0200

    bash: configure fzf to use fd (that use smart excludes)

--- bash.d/zzz-fzf.sh
+++ bash.d/zzz-fzf.sh
@@ -4,4 +4,9 @@
 FZF_CTRL_R_EDIT_KEY=ctrl-e
 FZF_CTRL_R_EXEC_KEY=enter
 FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS:+" "}--no-mouse"
+# fd interprets .gitignore by default
+# NOTE: it does not work for completion (C-T), because of unset FZF_DEFAULT_COMMAND in /usr/share/fzf/completion.bash
+if command -v fd >& /dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow'
+fi
 source /etc/fzf-plugins/history-exec/history-exec.bash
