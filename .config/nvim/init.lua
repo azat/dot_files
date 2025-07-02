6746ff1fb55779e1f78a82028540a7dee95fb423
Azat Khuzhin <a3at.mail@gmail.com>
2025-07-02 20:59:15 +0200

    nvim: catppuccin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -465,16 +465,13 @@ require('lazy').setup({
   },
 
   {
-    'folke/tokyonight.nvim',
+    "catppuccin/nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false },
-        },
+      require("catppuccin").setup {
+        no_italic = true
       }
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
 
