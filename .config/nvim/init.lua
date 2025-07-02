1dad2233ffda4785f9450d2249504a09af80b6bd
Azat Khuzhin <a3at.mail@gmail.com>
2025-07-02 21:03:13 +0200

    nvim: fix <C-w>w

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -93,6 +93,8 @@ vim.keymap.set('n', '<leader>n', '<cmd>set number!<CR>', { desc = 'Exit terminal
 vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
 vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
 
+vim.keymap.set('n', '<C-w><C-w>', '<C-w><C-p>', { noremap = true, silent = true })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
