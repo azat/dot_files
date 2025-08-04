669ed1d68cf728fd0264eff1bdc7b1af25a61321
Azat Khuzhin <a3at.mail@gmail.com>
2025-08-05 00:26:42 +0200

    bash: add comments for K9S_FEATURE_GATE_NODE_SHELL=true

--- bash.d/k8s.sh
+++ bash.d/k8s.sh
@@ -1,2 +1,7 @@
-# https://github.com/derailed/k9s/issues/1001
+# This workaround does not always work, I tried to run after/before/during k9s,
+# and I don't always see "Shell" button.
+#
+#    find ~/.xdg-XXX/k9s/clusters/ -name config.yaml | xargs -t -n1 yq -i '.k9s.featureGates.nodeShell = true'
+#
+# Refs: https://github.com/derailed/k9s/issues/1001
 export K9S_FEATURE_GATE_NODE_SHELL=true
